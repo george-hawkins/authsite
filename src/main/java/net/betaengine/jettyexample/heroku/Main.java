691819ed0aedbda5cea2cd5e052699fea1105c4c
George Hawkins <george-hawkins@users.noreply.github.com>
2015-01-16 19:22:08 +0100

    Initial checkin of login logic using HashLoginService.

--- src/main/java/net/betaengine/jettyexample/heroku/Main.java
+++ src/main/java/net/betaengine/jettyexample/heroku/Main.java
@@ -1,14 +1,21 @@
 package net.betaengine.jettyexample.heroku;
 
+import static net.betaengine.jettyexample.Util.unchecked;
+
+import java.io.IOException;
 import java.util.Optional;
 
+import net.betaengine.jettyexample.Util;
+
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.xml.XmlConfiguration;
 
 /**  Launch the web application in an embedded Jetty container. */
 public class Main {
-    private final static String WEBAPP_PATH = "src/main/webapp/";
-    private final static String WEBXMPL_PATH = WEBAPP_PATH + "/WEB-INF/web.xml";
+    private final static String WEBAPP_PATH = "src/main/webapp";
+    private final static String WEBXML_PATH = WEBAPP_PATH + "/WEB-INF/web.xml";
     private final static String PORT_ENV = "PORT";
     private final static int DEFAULT_PORT = 8080;
     
@@ -33,16 +40,20 @@ public class Main {
     }
     
     private static WebAppContext createRoot() {
-        WebAppContext root = new WebAppContext();
+        try {
+            System.setProperty("jetty.base", "target/classes");
+            Resource rootContextXml = Resource.newSystemResource("root-context.xml");
+            XmlConfiguration configuration = new XmlConfiguration(rootContextXml.getInputStream());
+            WebAppContext root = (WebAppContext)configuration.configure();
+            
+            // Use standard JSE class loading priority rather than prioritizing WEB-INF/[lib|classes].
+            // See http://www.eclipse.org/jetty/documentation/current/jetty-classloading.html
+            root.setParentLoaderPriority(true);
 
-        // Use standard JSE class loading priority rather than prioritizing WEB-INF/[lib|classes].
-        // See http://www.eclipse.org/jetty/documentation/current/jetty-classloading.html
-        root.setParentLoaderPriority(true);
-
-        root.setDescriptor(WEBXMPL_PATH);
-        root.setResourceBase(WEBAPP_PATH);
-        
-        return root;
+            return root;
+        } catch (Exception e) {
+            throw unchecked(e);
+        }
     }
     
     /** Returns the port specified by the environment variable PORT or DEFAULT_PORT if not present. */
