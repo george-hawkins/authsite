63487ea2b608830fa30085cd6fec3a797212dae6
George Hawkins <george-hawkins@users.noreply.github.com>
2015-01-27 14:37:33 +0100

    Renamed user-status.js to authsite.js and moved getURLParameter(...) function there.

---
+++ src/main/webapp/static/js/authsite.js
@@ -0,0 +1,38 @@
+//
+// This logic expects to find a <li> tags with id 'nav-user-login' and 'nav-user-name'.
+// It queries the service at '/userStatus' and creates either a login link or the user's name and a logout link.
+//
+(function ($) {
+    var loginTag = $('#nav-user-login');
+
+    if (loginTag.length != 0) {
+        function unhide(tag) {
+            tag.removeClass('hidden');
+            return tag;
+        }
+
+        function setupLoginTag(link, t) {
+            unhide(loginTag).find('a').prop('href', link).text(t);
+        }
+
+        $.getJSON('/userStatus', function(data) {
+            if (data.isLoggedIn) {
+                unhide($('#nav-user-name')).find('a').text(data.fullName);
+                setupLoginTag('/logout', 'Sign out');
+            } else {
+                var isLoginPage = $('form[action=j_security_check').length != 0;
+
+                if (isLoginPage) {
+                    loginTag.addClass('active');
+                }
+
+                setupLoginTag('/login.html', 'Sign in');
+            }
+        });
+    }
+})(jQuery);
+
+// http://stackoverflow.com/a/8764051/245602
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
+}
